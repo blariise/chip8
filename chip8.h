14c8ef3fea204edfd518ff3cc3a944e73aef9c9e
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-10 12:26:16 +0200

    cleanup header and source file

--- chip8.h
+++ chip8.h
@@ -1,26 +1,36 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+#include <string_view>
+#include <random>
+#include <chrono>
+#include <fstream>
+#include <array>
+#include <stdint.h>
+
 typedef struct Chip8 {
   std::array<uint8_t, 4096> memory;  // memory of 4kB
   std::array<uint8_t, 16> V;     // cpu register from V0 - VF
-  uint8_t SP; // level of the stack (stack pointer)
+  std::uint8_t SP; // level of the stack (stack pointer)
+  std::array<uint16_t, 16> stack;
 
-  uint16_t PC; // pointer to current instruction in memory (program counter)
-  uint16_t I; // pointer to location in memory (index register)
+  std::uint16_t PC; // pointer to current instruction in memory (program counter)
+  std::uint16_t I; // pointer to location in memory (index register)
+  std::uint16_t opcode;
+
 
   std::array<uint8_t, 64 * 32> display; // display 64x32 px
-  uint8_t delay_timer;
-  uint8_t sound_timer;
+  std::uint8_t delay_timer;
+  std::uint8_t sound_timer;
 
   /* 
     std::default_random_engine seed;
-    std::uniform_int_distribution<unsigned char> randomByte;
+    std::uniform_int_distribution<unsigned char> random_byte;
   */
 
-  Chip8 init(); // Initializing and reseting machine
+  void init(); // Initializing and reseting machine
   void cycle(); // Fetch, Decode, Execute (One cycle)
-  void loadRom(char const* filename);
+  //void loadRom(std::string_view filename);
 
 } Chip8;
 #endif // CHIP_8_H
