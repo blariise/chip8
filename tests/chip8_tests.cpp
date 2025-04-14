462d23b60c462c0539548d41899c5463313792b3
blarrise <61951559+blariise@users.noreply.github.com>
2025-04-14 20:49:30 +0200

    add tests up to 0x8nnn

--- tests/chip8_tests.cpp
+++ tests/chip8_tests.cpp
@@ -53,6 +53,7 @@ TEST_F(Chip8test, CALL_nested) {
   EXPECT_EQ(chip.stack[chip.SP], 0x102);
 }
 
+// 3xkk
 TEST_F(Chip8test, SE_Vx_Byte) {
   chip.PC = 0x100;
   chip.V[3] = 0x52;
@@ -62,4 +63,177 @@ TEST_F(Chip8test, SE_Vx_Byte) {
   // V[x] != 0x54, so PC increments only by 2
   loadOpcode(0x354);
   EXPECT_EQ(chip.PC, 0x106);
+}
+
+// 4xkk
+TEST_F(Chip8test, SNE_Vx_Byte) {
+  chip.PC = 0x100;
+  chip.V[8] = 0x43;
+  loadOpcode(0x483A);
+  EXPECT_EQ(chip.PC, 0x104);
+
+  loadOpcode(0x4843);
+  EXPECT_EQ(chip.PC, 0x106);
+}
+
+// 5xy0
+TEST_F(Chip8test, SE_Vx_Vy) {
+  chip.PC = 0x100;
+  chip.V[6] = 0x4D;
+  chip.V[4] = 0x4D;
+  loadOpcode(0x5640);
+  EXPECT_EQ(chip.PC, 0x104);
+
+  chip.V[7] = 0x24;
+  loadOpcode(0x5670);
+  EXPECT_EQ(chip.PC, 0x106);
+}
+
+// 6xkk
+TEST_F(Chip8test, LD_Vx_Byte) {
+  chip.V[2] = 0xBD;
+  loadOpcode(0x62AD);
+  EXPECT_EQ(chip.V[2], 0xAD);
+}
+
+// 7xkk
+TEST_F(Chip8test, ADD_Vx_Byte) {
+  chip.V[0xA] = 0x53;
+  loadOpcode(0x7A4A);
+  // 0x53 + 0x4A = 0x9D
+  EXPECT_EQ(chip.V[0xA], 0x9D);
+
+  loadOpcode(0x7AEE);
+  // 0x9D + 0xEE = 18B, but with overflow 18B = 395, 395 % 256 = 139 = 8B
+  EXPECT_EQ(chip.V[0xA], 0x8B);
+}
+
+// 8xy0
+TEST_F(Chip8test, LD_Vx_Vy) {
+  chip.V[0xD] = 0x6D;
+  chip.V[0x1] = 0x23;
+  loadOpcode(0x8D10);
+  EXPECT_EQ(chip.V[0xD], 0x23);
+}
+
+// 8xy1
+TEST_F(Chip8test, OR_Vx_Vy) {
+  chip.V[0xD] = 0x5A;
+  chip.V[0x1] = 0x3C;
+  loadOpcode(0x8D11);
+  EXPECT_EQ(chip.V[0xD], 0x7E);
+
+  chip.V[0xD] = 0x0F;
+  chip.V[0x1] = 0xF0;
+  loadOpcode(0x8D11);
+  EXPECT_EQ(chip.V[0xD], 0xFF);
+}
+
+// 8xy2
+TEST_F(Chip8test, AND_Vx_Vy) {
+  chip.V[0xD] = 0x5A;
+  chip.V[0x1] = 0x3C;
+  loadOpcode(0x8D12);
+  EXPECT_EQ(chip.V[0xD], 0x18);
+
+  chip.V[0xD] = 0xF0;
+  chip.V[0x1] = 0x0F;
+  loadOpcode(0x8D12);
+  EXPECT_EQ(chip.V[0xD], 0x00);
+}
+
+// 8xy3
+TEST_F(Chip8test, XOR_Vx_Vy) {
+  chip.V[0xD] = 0x5A;
+  chip.V[0x1] = 0x3C;
+  loadOpcode(0x8D13);
+  EXPECT_EQ(chip.V[0xD], 0x66);
+
+  chip.V[0xD] = 0xFF;
+  chip.V[0x1] = 0x0F;
+  loadOpcode(0x8D13);
+  EXPECT_EQ(chip.V[0xD], 0xF0);
+}
+
+// 8xy4
+TEST_F(Chip8test, ADD_Vx_Vy) {
+  chip.V[0xD] = 0xC7;
+  chip.V[0x1] = 0x9E;
+  loadOpcode(0x8D14);
+  EXPECT_EQ(chip.V[0xD], 0x65);
+  EXPECT_EQ(chip.V[0xF], 0x1);
+
+  chip.V[0xD] = 0xAB;
+  chip.V[0x1] = 0xF2;
+  loadOpcode(0x8D14);
+  EXPECT_EQ(chip.V[0xD], 0x9D);
+  EXPECT_EQ(chip.V[0xF], 0x1);
+
+  chip.V[0xD] = 0x3A;
+  chip.V[0x1] = 0x47;
+  loadOpcode(0x8D14);
+  EXPECT_EQ(chip.V[0xD], 0x81);
+  EXPECT_EQ(chip.V[0xF], 0x0);
+}
+
+// 8xy5
+TEST_F(Chip8test, SUB_Vx_Vy) {
+  chip.V[0xD] = 0x50;
+  chip.V[0x1] = 0x20;
+  loadOpcode(0x8D15);
+  EXPECT_EQ(chip.V[0xD], 0x30);
+  EXPECT_EQ(chip.V[0xF], 0x1);
+
+  chip.V[0xD] = 0x10;
+  chip.V[0x1] = 0x30;
+  loadOpcode(0x8D15);
+  EXPECT_EQ(chip.V[0xD], 0xE0);
+  EXPECT_EQ(chip.V[0xF], 0x0);
+
+  chip.V[0xD] = 0x77;
+  chip.V[0x1] = 0x77;
+  loadOpcode(0x8D15);
+  EXPECT_EQ(chip.V[0xD], 0x00);
+  EXPECT_EQ(chip.V[0xF], 0x0);
+}
+
+// 8xy6
+TEST_F(Chip8test, SHR_Vx) {
+  chip.V[0xD] = 0xB5;
+  loadOpcode(0x8D16);
+  EXPECT_EQ(chip.V[0xD], 0x5A);
+  EXPECT_EQ(chip.V[0xF], 0x1);
+
+  chip.V[0xD] = 0x4;
+  loadOpcode(0x8D16);
+  EXPECT_EQ(chip.V[0xD], 0x2);
+  EXPECT_EQ(chip.V[0xF], 0x0);
+}
+
+// 8xy7
+TEST_F(Chip8test, SUBN_Vx_Vy) {
+  chip.V[0xD] = 0x30;
+  chip.V[0x1] = 0x80;
+  loadOpcode(0x8D17);
+  EXPECT_EQ(chip.V[0xD], 0x50);
+  EXPECT_EQ(chip.V[0xF], 0x1);
+
+  chip.V[0xD] = 0xF0;
+  chip.V[0x1] = 0x40;
+  loadOpcode(0x8D17);
+  EXPECT_EQ(chip.V[0xD], 0x50);
+  EXPECT_EQ(chip.V[0xF], 0x0);
+}
+
+// 8xyE
+TEST_F(Chip8test, SHL_Vx) {
+  chip.V[0xD] = 0x8B;
+  loadOpcode(0x8D1E);
+  EXPECT_EQ(chip.V[0xD], 0x16);
+  EXPECT_EQ(chip.V[0xF], 0x1);
+
+  chip.V[0xD] = 0x7B;
+  loadOpcode(0x8D1E);
+  EXPECT_EQ(chip.V[0xD], 0xF6);
+  EXPECT_EQ(chip.V[0xF], 0x0);
 }
